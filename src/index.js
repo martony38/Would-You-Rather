b810131ff6b59e50d33e1fd4fce0cbcd1de9cc50
martony38 <martony@gmail.com>
2018-04-26 11:37:23 -0400

    chore: Use bootstrap npm package instead of CDN

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
